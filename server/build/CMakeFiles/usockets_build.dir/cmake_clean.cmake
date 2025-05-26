8a8fe2aa7322db8cf52c2e3dea8bf4ccf4027b72
Grace Wilson <gwilson011@gmail.com>
2025-05-25 23:13:08 -0600

    Update .gitignore and untrack node_modules, build, and vscode settings

--- server/build/CMakeFiles/usockets_build.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/Users/gracewilson/dev/libs/uWebSockets/uSockets/libusockets.a"
-  "CMakeFiles/usockets_build"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/usockets_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
